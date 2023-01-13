a3c1cb1df3b4f2f0b5e5dd8370266883a1b101b6
imvipp <120144946+imvipp@users.noreply.github.com>
2023-01-13 10:57:11 -0500

    Update box_autofarm.lua

--- shitware/calistreets/box_autofarm.lua
+++ shitware/calistreets/box_autofarm.lua
@@ -16,9 +16,10 @@ if game.PlaceId == 11998813375 then
                 if _G.box_AF == true then
                     character.HumanoidRootPart.CFrame = CFrame.new(28.6322002, 3.40753126, 141.114059, 0.409054905, 4.89856724e-08, -0.912509799, -1.10760638e-07, 1, 4.03118161e-09, 0.912509799, 9.9421186e-08, 0.409054905)
                     fireproximityprompt(game:GetService("Workspace").game.interactive.jobs.TruckJob.Truck.MeshPart.Attachment.ProximityPrompt)
-                    wait()
+                    wait(.1)
                     character.HumanoidRootPart.CFrame = CFrame.new(15.2930155, 3.42577648, 75.869606, -0.986088037, 2.45843186e-08, -0.166223839, 2.44509835e-08, 1, 2.84855317e-09, 0.166223839, -1.255412e-09, -0.986088037)
                     fireproximityprompt(game:GetService("Workspace").game.interactive.jobs.TruckJob.Shelves["Warehouse Shelf 1"].Attachment.ProximityPrompt)
+                    wait(.1)
                 else
                     check()
                     break
